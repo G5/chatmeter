6b7ae7c51911bf9fd48af8a2a6c1cf0bf7a683f1
Levi Brown <levi.brown@getg5.com>
2018-05-16 15:42:59 -0700

    create campaign should expect return 201

--- lib/chatmeter/api/campaign.rb
+++ lib/chatmeter/api/campaign.rb
@@ -13,10 +13,10 @@ module Chatmeter
     # POST /reviewBuilder/campaign/create
     def create_new_campaign(params)
       request(
-        expects:  200,
+        expects:  201,
         method:   :post,
         path:     "/reviewBuilder/campaign/create",
-        body:    params.to_json
+        body:     params.to_json
       )
     end
 
@@ -27,34 +27,34 @@ module Chatmeter
         method:   :get,
         path:     "/reviewBuilder/campaign/get/#{campaign_id}"
       )
-		end
-		
-		# PUT /reviewBuilder/campaign/{campaignId}
-		def update_campaign(campaign_id, params)
-			request(
-				expects:  200,
-				method:   :put,
-				path:     "/reviewBuilder/campaign/#{campaign_id}",
-				body:     params.to_json
-			)
-		end
-		
-		# GET /reviewBuilder/campaign
-		def search_campaign(params={})
-			request(
-				expects:  200,
-				method:   :get,
-				path:     "/reviewBuilder/campaign?#{params.to_query}"
-			)
-		end
+    end
 
-		# DELETE /reviewBuilder/campaign/delete/{campaignId}
-		def delete_campaign(campaign_id)
-			request(
-				expects:  200,
-				method:   :delete,
-				path:     "/reviewBuilder/campaign/delete/#{campaign_id}"
-			)
-		end
+    # PUT /reviewBuilder/campaign/{campaignId}
+    def update_campaign(campaign_id, params)
+      request(
+        expects:  200,
+        method:   :put,
+        path:     "/reviewBuilder/campaign/#{campaign_id}",
+        body:     params.to_json
+      )
+    end
+
+    # GET /reviewBuilder/campaign
+    def search_campaign(params={})
+      request(
+        expects:  200,
+        method:   :get,
+        path:     "/reviewBuilder/campaign?#{params.to_query}"
+      )
+    end
+
+    # DELETE /reviewBuilder/campaign/delete/{campaignId}
+    def delete_campaign(campaign_id)
+      request(
+        expects:  200,
+        method:   :delete,
+        path:     "/reviewBuilder/campaign/delete/#{campaign_id}"
+      )
+    end
   end
 end
