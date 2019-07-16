3382d0e8a592a91e1b361184c2c68efd81cb282b
Levi Brown <levi.brown@getg5.com>
2019-07-16 13:45:24 -0700

    typo

--- spec/chatmeter/api/account_spec.rb
+++ spec/chatmeter/api/account_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Chatmeter::API do
       expect(accounts).to include "232323"
     end
 
-    it "shold return valid response for #update_account" do
+    it "should return valid response for #update_account" do
       accounts = chatmeter.update_account("232323",{accountName: "New Account Name", clientAccountId: "new-client-account-id"})
       expect(accounts[:id]).to eq "232323"
       expect(accounts[:accountName]).to eq "New Account Name"
